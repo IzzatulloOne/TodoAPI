76f71c543d6683b73761ba4f56a1839db92b5d74
Dark <izzxolmatov14@gmail.com>
2025-12-03 19:56:30 +0500

    todo_api_v1,4

--- Todo/settings.py
+++ Todo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-o7*43xc!+mszw=9o@vaj7ci6*5@_fb8%m^-7otup=%mu2fx4k4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
